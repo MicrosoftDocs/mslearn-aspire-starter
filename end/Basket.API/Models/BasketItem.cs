185d7ae32951aeb2e01a02ea9553e32c3848f84a
AJ Matthews <alistairm@contentmaster.com>
2024-04-24 11:59:04 +0100

    Added sample code for telemetry module

--- end/Basket.API/Models/BasketItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eShop.Basket.API.Models;
-
-public class BasketItem
-{
-    public int ProductId { get; set; }
-
-    public int Quantity { get; set; }
-}
---
+++ start/build.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+dotnet build .\build\Build.proj
